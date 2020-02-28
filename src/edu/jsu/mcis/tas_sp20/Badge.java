d60bfccb002b11b6dd2a4993b32601799f39d53a
cmartin25 <cmartin25@stu.jsu.edu>
2020-02-28 11:56:10 -0600

    Started work on Badge class

--- src/edu/jsu/mcis/tas_sp20/Badge.java
+++ src/edu/jsu/mcis/tas_sp20/Badge.java
@@ -2,4 +2,23 @@ package edu.jsu.mcis.tas_sp20;
 
 public class Badge {
     
+    private String badgeDescription;
+    private String badgeID;
+    
+    public Badge(String ID, String badgeD) {
+        
+        this.badgeID = ID;
+        this.badgeDescription = badgeD;
+        
+    }
+    
+    public String getBadgeID(){
+        return badgeID;
+    }
+    
+    public String getBadgeDescription() {
+        return badgeDescription;
+    }
+    
 }
+
